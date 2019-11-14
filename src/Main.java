69154a872c5f1409cbe61a6a2516a6ac3a014ca4
Boris2209 <boris.litvinenko@bk.ru>
2019-11-14 21:04:32 +0300

    Выполнено первое задание

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ags){
+
+        Task_1 t1 = new Task_1();
+
+        String main_menu_text = "1. Задание 1: Класс File\n2. Задание 2\n3. Задание 3\n0. Выход\n";
+        int job_program = -1;
+        while (job_program!=0){
+            System.out.println(main_menu_text);
+            Scanner m_sc = new Scanner(System.in);
+            job_program = m_sc.nextInt();
+            switch (job_program){
+                case 1:
+                    t1.start_task_1();
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                default:
+                    System.out.println("Введена неверная команда, повторите ввод");
+                    break;
+            }
+        }
+    }
+
+
+}
